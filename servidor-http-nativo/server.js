6fc4f8c7d9676a73d03273c5de86bf150cf4960e
Luis Gustavo <lugubagi@gmail.com>
2026-08-14 21:28:30 +0000

    feat: funcionalidade de mensagem para determinadas páginas.

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -1,31 +1,35 @@
 import http from 'node:http'
+import { URL } from 'node:url'
 
 const porta = 3000
 
-const server = http.createServer();
+const server = http.createServer()
 
-const requisicao = (req,res) => {
+const requisicao = (req, res) => {
+    console.log(`Requisição registrada! ${req.method} ${req.url}`);
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200
+    const urlObj = new URL(req.url, `http://${req.headers.host}`);
 
-    console.log('Recurso criado! ${req.method} ${req.url}');
-    console.log(new Date().toISOString());
 
-    res.statusCode = 201
-    res.setHeader('Content-Type', 'text/plain', 'charset=utf-8');
-    res.end('acho q foih');
+    if (req.method === 'GET' && urlObj.pathname === '/contato') {
+        const contato = urlObj.searchParams.get('contato');
+        return res.end(JSON.stringify({"data": [{"telefone": "4002-8922", "Email": "pirilampo@gmail.com"}]}));
+
+    } else if(req.method === 'GET' && urlObj.pathname === '/produtos') {
+        const product = urlObj.searchParams.get('produto');
+        return res.end(JSON.stringify({"produto": "Detergente (produto mutável)"}));
+    } 
+    else if(req.method === 'GET' && urlObj.pathname === '/inexistente'){
+        res.statusCode = 404
+        return res.end("deu erro florzinha - 404")
+    }
+    return res.end(JSON.stringify({ "data": "página inicial" }));
+    res.end();
 }
 
 server.on('request', requisicao);
 
-server.listen(porta, ()=> {
-    console.log('Servidor ouvindo na porta ${porta}')
-})
-
-//feat - feature adicionada
-
-//core - configuração do projeto
-
-//add - adição de algum código de lógica
-
-//edit - edição de código
-
-//remove - remoção de código
+server.listen(porta, () => {
+    console.log(`Servidor ouvindo na porta ${porta}`)
+});
