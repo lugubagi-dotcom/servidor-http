0a4b3a192c9afc6463a3ebcd8890efce08386204
Luis Gustavo <lugubagi@gmail.com>
2026-08-14 18:11:22 +0000

    core: added script for execution

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -1,13 +1,20 @@
-const http = require('node:http')
-const porta = 3000;
-const server = http.createServer()
-server.on('request', (req, res) => {
-    console.log(`Servidor funcionando! ${req.method} ${req.url}`);
+import http from 'node:http'
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    res.end('Servidor Funcionando!')
+const porta = 3000
+
+const server = http.createServer();
+
+const requisicao = (req,res) => {
+
+    console.log('Requisição recebida! ${req.method} ${req.url}');
+
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json;');
+    res.end(JSON.stringify({"chave":"valor"}));
+}
+
+server.on('request', requisicao);
+
+server.listen(porta, ()=> {
+    console.log('Servidor ouvindo na porta ${porta}')
 });
-server.listen(porta, () => {
-    console.log(`Servidor ouvindo na porta ${porta}`)
-});;
